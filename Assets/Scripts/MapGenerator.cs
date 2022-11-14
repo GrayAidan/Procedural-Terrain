ab813d083e8cb99f40add6eda853ddf37c20ce34
GrayAidan <114709817+GrayAidan@users.noreply.github.com>
2022-11-14 14:07:18 -0500

    Milestone - Part 5
    World is build. Mesh is built. Height map and texturing is working.

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public enum Drawmode
+    {
+        NoiseMap,
+        ColorMap,
+        Mesh
+    }
+
+    public Drawmode drawMode;
+    
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
@@ -13,8 +22,13 @@ public class MapGenerator : MonoBehaviour
     public float persistance;
     public float lacunarity;
 
+    public TerrainType[] regions;
+
     public int seed;
     public Vector2 offset;
+
+    public float meshHeightMutiplier;
+    public AnimationCurve meshHeightCurve;
 
     public bool autoUpdate;
 
@@ -22,9 +36,37 @@ public class MapGenerator : MonoBehaviour
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
-        MapDisplay display = FindObjectOfType<MapDisplay>();
 
-        display.DrawNoiseMap(noiseMap);
+        Color[] colorMap = new Color[mapWidth * mapHeight];
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for(int i = 0; i < regions.Length; i++)
+                {
+                    if(currentHeight <= regions[i].height)
+                    {
+                        colorMap[y * mapWidth + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if(drawMode == Drawmode.NoiseMap)
+        {
+            display.DrawTexture(TextureGen.TextureFromHeightMap(noiseMap));
+        }
+        else if (drawMode == Drawmode.ColorMap)
+        {
+            display.DrawTexture(TextureGen.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+        }
+        else if(drawMode == Drawmode.Mesh)
+        {
+            display.DrawMesh(MeshGen.GenerateTerrainMesh(noiseMap, meshHeightMutiplier, meshHeightCurve), TextureGen.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+        }
     }
 
     private void OnValidate()
@@ -48,6 +90,14 @@ public class MapGenerator : MonoBehaviour
         {
             octaves = 0;
         }
+    }
+
+    [System.Serializable]
+    public struct TerrainType
+    {
+        public string name;
+        public float height;
+        public Color colour;
     }
 
 }
--- Assets/Scripts/Old/FPSInput.cs
+++ Assets/Scripts/Old/FPSInput.cs
@@ -5,105 +5,48 @@ using UnityEngine;
 
 public class FPSInput : MonoBehaviour
 {
-    /*
-    This script handles the input of the player. It allows the player to jump around,
-    and if they double jump, they can fly up into the air.
-
-    I will add the functionality to move around and walk through the environment, 
-    as well as reset the player position to the middle so you can use world controls
-    */
-    
-    
-    public CharacterController controller; 
-    public GroundedCheck groundedBox; //the gameobject on the player that holds its grounded trigger
-    private Vector3 playerVelocity; //the velocity of the player that is used to move the character control
-    public float jumpHeight = 1.0f; //set publicly. the multiplier that sets the jump height
-    public float gravityValue = -9.81f; //set publicly. the multiplier that sets the player gravity, how fast they fall
-
-    public bool flying; //the bool that sets the current state of the player, changed by StateChange() if DoubleSpaceCheck() is running true
-    [HideInInspector]
-    public bool spaceClickCheck; //this bool is true for a short period after the jump button is pressed, if pressed again while true the player starts flying
+    public CharacterController controller;
+    public GroundedCheck groundedBox;
+    private Vector3 playerVelocity;
+    public float playerSpeed = 2.0f;
+    public float playerWalkSpeed = 2.0f;
+    public float playerSprintSpeed = 9.0f;
+    public float jumpHeight = 1.0f;
+    public float gravityValue = -9.81f;
 
     private void Start()
     {
-
     }
 
     void Update()
     {
-        if (flying) //depending on the flying bool, run the state function that handles the character controls
-        {
-            Flying();
-        }
-        else if (!flying)
-        {
-            Jumping();
-        }
-
-        StateChange(spaceClickCheck); //checks if the double clicked space is checked, takes in a bool value that allows the double click to happen
-    }
-
-    public void Jumping() //this state is when the player is walking around on the ground and jumping
-    {
-        if (groundedBox.grounded && playerVelocity.y < 0) //if the player is on the ground, dont keep adding gravity
+        if (groundedBox.grounded && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
 
-        if (Input.GetKeyDown("space")) //if the space bar is clicked, start the DoubleSpaceCheck()
-        {
-            StartCoroutine(DoubleSpaceCheck());
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = transform.TransformDirection(move);
+        controller.Move(move * Time.deltaTime * playerSpeed);
 
-            if (groundedBox.grounded) //if the player is on the ground, preform a jump
-            {
-                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-            }
+        if(Input.GetKey("left shift") && groundedBox.grounded)
+        {
+            playerSpeed = playerSprintSpeed;
+        }
+        else
+        {
+            playerSpeed = playerWalkSpeed;
         }
 
-        playerVelocity.y += gravityValue * Time.deltaTime; //sets the player velocity Y value to the public gravity value
-
-        controller.Move(playerVelocity * Time.deltaTime); //moves the player with the controller using the velocity value
-    }
-
-    public void Flying() //this state is when the player is flying
-    {
-        if (Input.GetKeyDown("space")) //if the player presses space, only start DoubleSpaceCheck() on one frame
+        // Changes the height position of the player..
+        if (Input.GetKeyDown("space") && groundedBox.grounded)
         {
-            StartCoroutine(DoubleSpaceCheck());
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            groundedBox.grounded = false;
         }
 
-        if (Input.GetKey("space")) //moves the player upwards if the spacebar is held down
-        {
-            playerVelocity.y = 6;
-        }
-        else if (Input.GetKey("left ctrl")) //moves the player downwards if the left control is held down
-        {
-            playerVelocity.y = -6;
-        }
-        else //if nothing is held, the player stays still
-        {
-            playerVelocity.y = 0;
-        }
+        playerVelocity.y += gravityValue * Time.deltaTime;
 
-        controller.Move(playerVelocity * Time.deltaTime); //moves the player with the controller using the velocity value
-    }
-
-    public void StateChange(bool canChange) //called from Update(). if the state can be changed, depending on DoubleSpaceCheck(), and the spacebar is pressed, the state changes
-    {
-        if (canChange && Input.GetKeyDown("space"))
-        {
-            flying = !flying;
-        }
-    }
-
-    public IEnumerator DoubleSpaceCheck() //if this is called, sets the spaceClickCheck as true, allowing StateChange() to change the state
-    {
-        yield return new WaitForSeconds(0.05f);
-
-        spaceClickCheck = true;
-
-        yield return new WaitForSeconds(0.5f);
-
-        spaceClickCheck = false;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
